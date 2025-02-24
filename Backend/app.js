4959299ee31742631f16d8410a5bbb3b8e077a3b
aryarajnishu <22bds007@iiitdwd.ac.in>
2025-02-24 14:06:23 +0530

    Added Backend files

---
+++ Backend/app.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const cookieParser = require('cookie-parser');
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectToDb = require('./db/db')
+
+const userRoutes = require('./routes/user.routers')
+const captainRoutes = require('./routes/captain.routes')  
+const mapsRoutes = require('./routes/maps.routes')
+const rideRoutes = require('./routes/ride.routes')
+
+connectToDb();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('what is doing');
+});
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+module.exports = app;
